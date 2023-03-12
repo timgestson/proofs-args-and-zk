730ec1ac59c2de040167b7033a824c2a99b8bd7a
Tim Gestson <tgestson@gmail.com>
2023-03-11 22:11:28 -0500

    Reorganize utils (#4)

--- util.py
+++ util.py
@@ -1,10 +1,14 @@
 from hashlib import sha256
+from itertools import product
 from felt import Felt
 
 
 def hash_felt_list(lst):
     hash = sha256(bytearray([i.val % 256 for i in lst])).digest()
     return Felt(int.from_bytes(hash, "big"))
+
+def hypercube(n, F=Felt):
+    return [list(i) for i in product([F(0), F(1)], repeat=n)]
 
 
 def flatten(matrix):
